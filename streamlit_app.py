bb37bd2a4d40f2d95b372f31bb6e718a72a3b969
johntsnowflake <john.tay@icare.nsw.gov.au>
2026-06-18 11:41:34 +1000

    Update streamlit_app.py
    include requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,28 +9,13 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """)
 
-
-#option = st.selectbox(
-#    "What is your favorite fruit?",
-#    ("Banana", "Strawberries", "Peaches"),
-#)
-
-#st.write("Your favorite fruit is:", option)
-
-#import streamlit as st
-
 name_on_order = st.text_input('Name of Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
-
-
-#session = get_active_session()
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-#my_dataframe = session.table("smoothies.public.fruit_options")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -38,32 +23,15 @@ ingredients_list = st.multiselect(
     , max_selections=5
     )
 
-
 if ingredients_list:
-#    st.write(ingredients_list)
-#    st.text(ingredients_list)
 
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-#    st.write(ingredients_string)
-
-
-#    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-#                    values ('""" + ingredients_string + """')"""
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-    
-#    st.write(my_insert_stmt)
-#    st.stop()
-
-#    if ingredients_string:
-#        session.sql(my_insert_stmt).collect()
-#        st.success('Your Smoothie is ordered!', icon="✅")
 
     time_to_insert = st.button('Submit Order')
     
@@ -71,6 +39,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 
